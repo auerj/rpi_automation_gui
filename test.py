ecc70a4e6dc1ccb7b77ddb2f0e23fee813bbf312
Jakob Auer <jakob@auer.tech>
2020-01-17 16:46:44 +0100

    button callbacks

--- test.py
+++ test.py
@@ -5,7 +5,19 @@ class Ui(QtWidgets.QDialog):
     def __init__(self):
         super(Ui, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi('test.ui', self) # Load the .ui file
+        
+        button_plus = self.findChild(QtWidgets.QPushButton, "temp_plus")
+        button_plus.clicked.connect(self.button_pressed)
+
+
+        button_minus = self.findChild(QtWidgets.QPushButton, "temp_minus")
+        button_minus.clicked.connect(self.button_pressed)
+
         self.show() # Show the GUI
+
+    def button_pressed(self):
+        print("Button pressed")
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
