e3aa1189e0fbbff85f55197c5bea521402bd60fe
Jakob Auer <jakob@auer.tech>
2020-01-17 15:48:34 +0100

    first experiments

---
+++ test.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+class Ui(QtWidgets.QDialog):
+    def __init__(self):
+        super(Ui, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('test.ui', self) # Load the .ui file
+        self.show() # Show the GUI
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
